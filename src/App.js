30beba6204a430ad9a5f40a66b7a39c22cc2783d
NolzMitHolz <nils-lehmann@outlook.de>
2024-07-18 21:23:27 +0200

    added tsconfig.json to support typescript (.tsx)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-function App() {
-  return (
-    <div className="App">
-     Hello World
-    </div>
-  );
-}
+import Frontpage from "./components/sites/Frontpage";
+import Projects from "./components/sites/Projects";
 
-export default App;
+export default function App() {
+    return (
+        <div className="main-container">
+            <Frontpage/>
+            <Projects/>
+        </div>
+    );
+}
---
+++ src/components/sites/Projects.tsx
@@ -0,0 +1,7 @@
+export default function Projects(): JSX.Element {
+    return (
+        <div>
+            Hello World!
+        </div>
+    )
+}
